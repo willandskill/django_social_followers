91728c873b890011dce715f921cfa73cad0fea76
hassanmian <mian@willandskill.se>
2015-11-17 14:11:07 +0100

    Correct download url

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'django_social_followers',
     packages = ['django_social_followers'], # this must be the same as the name above
-    version = '0.1',
+    version = '0.4',
     description = 'Get followers from Social Media channels',
     author = 'Will And Skill AB - Sweden',
     author_email = 'info@willandskill.se',
     url = 'https://github.com/willandskill/django_social_followers', # use the URL to the github repo
-    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.3', # I'll explain this in a second
+    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.4', # I'll explain this in a second
     keywords = ['social', 'followers', 'instagram', 'facebook', 'twitter'], # arbitrary keywords
     classifiers = [],
     install_requires=[

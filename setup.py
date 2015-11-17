cb3e79ca3a6199c475269b89174ca5ee5d0bb804
hassanmian <mian@willandskill.se>
2015-11-17 14:30:29 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 setup(
     name = 'django_social_followers',
     packages = ['django_social_followers'], # this must be the same as the name above
-    version = '0.7',
+    version = '0.8',
     description = 'Get followers from Social Media channels',
     author = 'Will And Skill AB - Sweden',
     author_email = 'info@willandskill.se',
     url = 'https://github.com/willandskill/django_social_followers', # use the URL to the github repo
-    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.7', # I'll explain this in a second
+    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.8', # I'll explain this in a second
     keywords = ['social', 'followers', 'instagram', 'facebook', 'twitter'], # arbitrary keywords
     classifiers = [],
     install_requires=[
-        'django'
+        'django',
         'facebook-sdk',
         'tweepy',
         'python-instagram'

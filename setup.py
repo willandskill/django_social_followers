b03afef9e06f3f0c1a5815d4663238fedcbfe111
hassanmian <mian@willandskill.se>
2015-11-17 14:17:09 +0100

    Correct requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name = 'django_social_followers',
     packages = ['django_social_followers'], # this must be the same as the name above
-    version = '0.4',
+    version = '0.5',
     description = 'Get followers from Social Media channels',
     author = 'Will And Skill AB - Sweden',
     author_email = 'info@willandskill.se',
     url = 'https://github.com/willandskill/django_social_followers', # use the URL to the github repo
-    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.4', # I'll explain this in a second
+    download_url = 'https://github.com/willandskill/django_social_followers/releases/tag/0.5', # I'll explain this in a second
     keywords = ['social', 'followers', 'instagram', 'facebook', 'twitter'], # arbitrary keywords
     classifiers = [],
     install_requires=[
